6749c11efd2d9c5481bb9bdfcd86f53cf81cd260
levtoshi <Intel@SATOSHI-LAPTOP>
2026-03-08 19:23:29 +0200

    add hw 04.03.2026

---
+++ HomeWork_04.03.2026/HomeWork_04.03.2026/Models/CryptoCurrency.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeWork_04._03._2026.Models
+{
+    public class CryptoCurrency
+    {
+        public int Rank { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public double Change1h { get; set; }
+        public double Change24h { get; set; }
+        public double Change7d { get; set; }
+        public long MarketCap { get; set; }
+        public long Volume24h { get; set; }
+        public string CirculatingSupply { get; set; } = string.Empty;
+    }
+}
