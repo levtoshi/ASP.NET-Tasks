2a6e1ef3ed996aa2519612196058fc21e297ddbc
levtoshi <Intel@SATOSHI-LAPTOP>
2026-03-08 22:21:34 +0200

    add hw 06.03.2026

---
+++ HomeWork_06.03.2026/HomeWork_06.03.2026/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork_06._03._2026.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Author { get; set; } = "";
+        public string Description { get; set; } = "";
+        public double Price { get; set; }
+        public string ImageUrl { get; set; } = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw1VvHQ7ZzfHWdBODYTF64oFMKI_fIVQ-sJg&s";
+    }
+}
---
+++ HomeWork_06.03.2026/HomeWork_06.03.2026/Services/BookService.cs
@@ -0,0 +1,24 @@
+﻿using HomeWork_06._03._2026.Models;
+
+namespace HomeWork_06._03._2026.Services
+{
+    public class BookService
+    {
+        public List<Book> Books { get; set; } = new();
+
+        public BookService()
+        {
+            for (int i = 1; i <= 25; i++)
+            {
+                Books.Add(new Book
+                {
+                    Id = i,
+                    Title = $"Book Title {i}",
+                    Author = "Author Name",
+                    Price = 250 + (i * 10),
+                    Description = "This is a detailed description of the book. It provides insights into the plot and characters."
+                });
+            }
+        }
+    }
+}
